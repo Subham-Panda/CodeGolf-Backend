7afe51ebfca6a0434a9cd1f6595fb4d44753625a
AJ1479 <arushijain147@gmail.com>
2020-09-30 17:11:17 +0530

    fix: Fixed leaderboard module

--- src/controllers/leaderboard.js
+++ src/controllers/leaderboard.js
@@ -1,15 +1,25 @@
+/* eslint-disable max-len */
 const Bull = require('bull');
 
 const Queue = new Bull('queue');
 
 const db = require('../models/models');
 
-exports.leaderboard = async (user, questionNo, time, sLength) => {
+const leaderboard = db.Leaderboard.find().toArray();
+const question = db.Question.find().toArray();
+const mainLeaderboard = leaderboard[leaderboard.findIndex((o) => o.questionName === 'Global')].users;
+const ranks = {};
+mainLeaderboard.forEach((item) => {
+    ranks.item.user = mainLeaderboard.indexOf(item.user);
+});
+
+exports.leaderboard = async (user, questionName, time, sLength, hasSolved) => {
     const data = {
         user,
-        questionNo,
+        questionName,
         time,
         sLength,
+        hasSolved,
 
     };
     await Queue.add(data);
@@ -24,7 +34,7 @@ function sorting(arr) {
         return compareTime(a, b);
     };
     const compareNo = (a, b) => {
-        if (a.questionsSolved !== b.questionsSolved) return a.questionsSolved - b.questionsSolved;
+        if (a.questionsSolved !== b.questionsSolved) return b.questionsSolved - a.questionsSolved;
 
         return compareLength(a, b);
     };
@@ -38,108 +48,88 @@ function sorting(arr) {
 }
 
 function task(job) {
-    const question = db.Question.findOne({ questionNo: job.questionNo });
-    const questionLeaderboard = db.Leaderboard.findOne({ questionNo: job.questionNo });
-    const leaderboard = db.Leaderboard.findOne({ questionNo: 0 }).users;
+    // 1. get questionLeaderboard, leaderboard
+    const { points } = question[question.findIndex((o) => o.questionName === job.questionName)];
+    const questionLeaderboard = leaderboard[leaderboard.findIndex((o) => o.questionName === job.questionName)].users;
 
-    // to calculate score for question
-    let bestLength;
-
-    if (questionLeaderboard.users.length !== 0) {
-        bestLength = questionLeaderboard.users[0].sLength;
-        if (bestLength > job.sLength) {
-            let newLeaderboard = leaderboard;
-            const newQleaderboard = [];
-            // updating every user's scores
-            questionLeaderboard.users.forEach((item) => {
-                const newScore = (job.sLength / item.sLength) * (question.points);
-                newQleaderboard.push({
-                    user: item.user,
-                    score: newScore,
-                    questionsSolved: item.questionsSolved,
-                    sLength: item.sLength,
-                    latestTime: item.latestTime,
-                });
-                // eslint-disable-next-line max-len
-                const leaderboardUser = leaderboard[leaderboard.findIndex((o) => o.user === item.user)];
-                newLeaderboard = newLeaderboard.filter((item1) => item1 !== leaderboardUser);
-                const updatedScore = leaderboardUser.score - item.score + newScore;
-                newLeaderboard.push({
-                    user: leaderboardUser.user,
-                    score: updatedScore,
-                    questionsSolved: leaderboardUser.questionsSolved,
-                    sLength: leaderboardUser.sLength,
-                    latestTime: leaderboardUser.latestTime,
-                });
-                sorting(newLeaderboard);
-                db.Leaderboard.findOneAndUpdate(
-                    { questionNo: 0 },
-                    { users: newLeaderboard },
-                );
-            });
-            db.Leaderboard.findOneAndUpdate(
-                { questionNo: job.questionNo },
-                { users: newQleaderboard },
-            );
-            bestLength = job.sLength;
-        }
-    } else {
+    // 2.  if its first submission, then add the user with 0 points
+    if (!job.hasSolved) {
+        questionLeaderboard.push({
+            user: job.user,
+            score: 0,
+            questionsSolved: 1,
+            sLength: 0,
+            latestTime: job.time,
+        });
+    }
+    // 3. check if code of current user shorter than best length
+    let bestLength = questionLeaderboard[0].sLength;
+    if (job.sLength < bestLength) {
         bestLength = job.sLength;
     }
-    const score = (bestLength / job.sLength) * (question.points);
+    //         b. Update everyone's scores in the question leaderboard, with a .map(u)
+    //                     store u's old_points in a variable
+    //                     if u == current user, then score = question.points
+    //                     if not , then score = formula(best length)
+    //             extract out ranks[u], using that, extract out the user object in the global leaderboard
+    //                     global_leaderboard = current_points - old_points + new_points
 
-    // updating score and questionsSolved
-    const mainUser = leaderboard[leaderboard.findIndex((o) => o.user === job.user)];
-    let newScore = mainUser.score;
-    let qSolved = mainUser.questionsSolved;
-    let length = mainUser.sLength;
-    for (let i = 0; i < questionLeaderboard.users.length; i += 1) {
-        if (questionLeaderboard.users[i].user === job.user) {
-            newScore -= questionLeaderboard.users[i].score;
-            qSolved -= 1;
-            length -= questionLeaderboard.users[i].sLength;
-            break;
+    questionLeaderboard.map((u) => {
+        const { user } = u;
+        const index = ranks.user;
+        let { sLength } = u;
+        const questionsSolved = mainLeaderboard[index].questionsSolved + u.questionsSolved;
+        let { latestTime } = mainLeaderboard[index];
+        let lTime = u.latestTime;
+        if (user === job.user) {
+            sLength = job.sLength;
+            latestTime = job.time;
+            lTime = job.time;
         }
-    }
-    newScore += score;
-    qSolved += 1;
-    length += job.sLength;
-
-    // updating leaderboards
-    let newQuestionLeaderboard = db.Leaderboard.findOne({ questionNo: job.questionNo }).users;
-    newQuestionLeaderboard = newQuestionLeaderboard.filter((item) => item.user !== job.user);
-    let newLeaderboard = db.Leaderboard.findOne({ questionNo: 0 }).users;
-    newLeaderboard = newLeaderboard.filter((item) => item.user !== job.user);
-
-    newQuestionLeaderboard.push({
-        user: job.user,
-        score,
-        questionsSolved: qSolved,
-        sLength: job.sLength,
-        latestTime: job.time,
+        const score = (bestLength / sLength) * points;
+        const totalLength = mainLeaderboard[index].sLength - u.sLength + sLength;
+        const totalScore = mainLeaderboard[index].score - u.score + score;
+        mainLeaderboard[index].users = {
+            user,
+            score: totalScore,
+            questionsSolved,
+            sLength: totalLength,
+            latestTime,
+        };
+        return {
+            user,
+            score,
+            questionsSolved: 0,
+            sLength,
+            latestTime: lTime,
+        };
     });
-    newLeaderboard.push({
-        user: job.user,
-        score: newScore,
-        questionsSolved: qSolved,
-        sLength: length,
-        latestTime: job.time,
+    // sort(gameLeaderboard)
+    sorting(mainLeaderboard);
+
+    // sort(questionleaderboard)
+    sorting(questionLeaderboard);
+
+    // UPDATE ranks map !!
+    mainLeaderboard.forEach((item) => {
+        ranks.item.user = mainLeaderboard.indexOf(item.user);
     });
 
-    sorting(newQuestionLeaderboard);
-
+    // db.update()
     db.Leaderboard.findOneAndUpdate(
-        { questionNo: job.questionNo },
-        { users: newQuestionLeaderboard },
+        { questionName: job.questionName },
+        { users: questionLeaderboard },
     );
 
-    sorting(newLeaderboard);
+    // db.update()
     db.Leaderboard.findOneAndUpdate(
-        { questionNo: 0 },
-        { users: newLeaderboard },
+        { questionName: 'Global' },
+        { users: mainLeaderboard },
     );
+
+    leaderboard[leaderboard.findIndex((o) => o.questionName === 'Global')].users = mainLeaderboard;
+    leaderboard[leaderboard.findIndex((o) => o.questionName === job.questionName)].users = questionLeaderboard;
 }
-
 Queue.process(async (job) => {
     task(job.data);
 });
