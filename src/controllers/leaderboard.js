68f9f628f72e09e198ddb7bf9c6bb2d5b621380c
AJ1479 <arushijain147@gmail.com>
2020-09-30 17:58:27 +0530

    fix: Fixed comments and ranks map in leaderboard module

--- src/controllers/leaderboard.js
+++ src/controllers/leaderboard.js
@@ -9,8 +9,8 @@ const leaderboard = db.Leaderboard.find().toArray();
 const question = db.Question.find().toArray();
 const mainLeaderboard = leaderboard[leaderboard.findIndex((o) => o.questionName === 'Global')].users;
 const ranks = {};
-mainLeaderboard.forEach((item) => {
-    ranks.item.user = mainLeaderboard.indexOf(item.user);
+mainLeaderboard.forEach((item, i) => {
+    ranks[item.user] = i;
 });
 
 exports.leaderboard = async (user, questionName, time, sLength, hasSolved) => {
@@ -48,11 +48,11 @@ function sorting(arr) {
 }
 
 function task(job) {
-    // 1. get questionLeaderboard, leaderboard
+    // get question and questionLeaderboard
     const { points } = question[question.findIndex((o) => o.questionName === job.questionName)];
     const questionLeaderboard = leaderboard[leaderboard.findIndex((o) => o.questionName === job.questionName)].users;
 
-    // 2.  if its first submission, then add the user with 0 points
+    // check for first submission
     if (!job.hasSolved) {
         questionLeaderboard.push({
             user: job.user,
@@ -62,18 +62,13 @@ function task(job) {
             latestTime: job.time,
         });
     }
-    // 3. check if code of current user shorter than best length
+    // check bestLength
     let bestLength = questionLeaderboard[0].sLength;
     if (job.sLength < bestLength) {
         bestLength = job.sLength;
     }
-    //         b. Update everyone's scores in the question leaderboard, with a .map(u)
-    //                     store u's old_points in a variable
-    //                     if u == current user, then score = question.points
-    //                     if not , then score = formula(best length)
-    //             extract out ranks[u], using that, extract out the user object in the global leaderboard
-    //                     global_leaderboard = current_points - old_points + new_points
 
+    // looping through every user who has solved that question in case bestLength changes including the current user
     questionLeaderboard.map((u) => {
         const { user } = u;
         const index = ranks.user;
@@ -89,7 +84,7 @@ function task(job) {
         const score = (bestLength / sLength) * points;
         const totalLength = mainLeaderboard[index].sLength - u.sLength + sLength;
         const totalScore = mainLeaderboard[index].score - u.score + score;
-        mainLeaderboard[index].users = {
+        mainLeaderboard[index] = {
             user,
             score: totalScore,
             questionsSolved,
@@ -110,9 +105,9 @@ function task(job) {
     // sort(questionleaderboard)
     sorting(questionLeaderboard);
 
-    // UPDATE ranks map !!
-    mainLeaderboard.forEach((item) => {
-        ranks.item.user = mainLeaderboard.indexOf(item.user);
+    // updating ranks
+    mainLeaderboard.forEach((item, i) => {
+        ranks[item.user] = i;
     });
 
     // db.update()
@@ -127,7 +122,6 @@ function task(job) {
         { users: mainLeaderboard },
     );
 
-    leaderboard[leaderboard.findIndex((o) => o.questionName === 'Global')].users = mainLeaderboard;
     leaderboard[leaderboard.findIndex((o) => o.questionName === job.questionName)].users = questionLeaderboard;
 }
 Queue.process(async (job) => {
